6e2d9b191d2810810bb1a57a6bc54328aed55bf6
altura1000 <altura11111@gmail.com>
2025-09-18 00:40:03 +0300

    add path config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // tells Next to make a static site in /out
+  basePath: '/shalva',       // repo name
+  assetPrefix: '/shalva',    // ensures CSS/img URLs work
   eslint: {
     ignoreDuringBuilds: true,
   },
